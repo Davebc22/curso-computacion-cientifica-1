7de74eb72d5caf5ad8f1281abcad7a2caf41a365
Davebc22 <ederd.barreroc@konradlorenz.edu.co>
2026-08-16 17:45:58 -0500

    Ex 3

--- taller/test_code.py
+++ taller/test_code.py
@@ -1,37 +1,47 @@
 """
-Realizar un algoritmo que transforme números binarios a números decimales
+Realizar un algoritmo que transforme numeros romanos a numeros decimales
+
+Consideraciones:
+1. Los símbolos se escriben de izquierda a derecha de MAYOR a MENOR valor
+2. Los números romanos representan solo enteros positivos
+3. De Mayor a menor: M=1000, D=500, C=100, L=50, X=10, V=5, I=1
+4. Si una letra está seguida por otra de igual o menor valor ENTONCES sus valores se suman. EJP: CX -> 100 + 10
+5. Si una letra está seguida por otra de mayor valor ENTONCES sus valores se restan. EJP: XC -> 100 - 10
 
 Algoritmo:
+1. Solicitar al usuario un número romano r
+2. Convertir r en una lista_romanos
+2.1 Convertir todos los elementos de lista_romanos en mayúsuclas
+3 Declarar un diccionario valores_romanos asociando cada letra al valor entero correspondiente
+4. Añadir a una lista_enteros los valores del diccionario asociados a cada letra en lista_romanos en orden 
+5.0 Si lista_enteros[i] > lista_enteros[i+1] 
+5.0.1 Entonces sumar lista_enteros[i] + lista_enteros[i+1]
+5.0.2 añadir el valor de la suma  5.0.1 a total
+5.1 Si lista_enteros[i] <= lista_enteros[i+1]
+5.1.1 Entonces restar lista_enteros[i+1] - lista_enteros[i]
+5.1.2 Entonces añadir el valor de la resta 5.1.1 a total 
+6.0 Imprimir valor total una vez terminado el ciclo
 
-1. Solicitar al usuario un número n expresado en forma binaria
-2. Convertir en una lista 1 el número n dado 
-3. Leer el número k de dígitos del número n dado
-4. Guardar en una lista 2 las potencias de 2 que van desde la potencia 0 hasta la potencia k-1
-4.1 Invertir orden de la lista
-5. Comparar los elementos de la lista 1 y lista 2:
-5.1 Si lista 1 (i) == 1 entonces guardar en lista 3 el elemento lista 2 (i)
-5.2 Si lista 1 (i) == 0 entonces descartar
-6. Sumar los elementos de lista 3
 
 """
 
 
-n='10010110000'
-# Comprensión de lista para convertir un string a una lista de enteros
-lista_n = [int(caracter) for caracter in n]
-k=len(lista_n)
-lista_p2 = [2**numero for numero in range(k)]
-lista_p2 = lista_p2[::-1]
-lista_final = []
+r = 'LXIV'
+lista_romanos = list(r)
+lista_romanos = [letra.upper() for letra in lista_romanos]
+lista_enteros = []
+#Utilización de diccionari0 para hacer el recorrido más rápido
+valores = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+total = 0
 
-for i,numero in enumerate(lista_n):
-    if numero == 1:
-        lista_final.append(lista_p2[i])
-
-print(lista_n)
-print(lista_p2)
-print(lista_final)
-lista_final = sum(lista_final)
-print(lista_final)
+#Recuperar valores del diccionario con comprensión de listas
+lista_enteros= [valores[letra] for letra in lista_romanos]
+#Detenerse en el penúltimo debido al uso del sucesor i+1
+for i,numero in enumerate(lista_enteros):
+    if i+1 < len(lista_enteros) and lista_enteros[i] < lista_enteros[i+1]:
+        total -= lista_enteros[i]
+    else:
+        total+= lista_enteros[i]
+print(total)
 
 
