a6a2a1a9b2d71d9c0b4df7af42de414c20237b4a
Davebc22 <ederd.barreroc@konradlorenz.edu.co>
2026-08-27 12:27:47 -0500

    cantidad de ocurrencias de un caracter

--- taller/test_code.py
+++ taller/test_code.py
@@ -1,20 +1,13 @@
 """
-# 5. Realiza un algoritmo que valide si una frase es un palindromo
-ejp: luz azul
-Algoritmo
-1. Pedir al usuario una palabra p
-2. Volver la palabra p una lista l1
-3. invertir lista l2
-4. Evaluar si l1 = l2
-4.1 Si sí: l1 es palíndromo
-4.2 si no: l1 no es palíndromo
-"""
-palabra = input('Inserte una palabra ')
-palabra.lower()
-lista1 = list(palabra.replace(" ", ""))
-lista2 = lista1[::-1]
+# 6. Realiza un algoritmo que cuente la cantidad de ocurrencias de cada caracter en una frase
 
-if lista1 == lista2:
-    print(f'La frase/palabra "{palabra}" SÍ es un palíndromo')
-else: 
-    print(f'La frase/palabra "{palabra}" NO es un palíndromo')
+
+"""
+
+palabra = input('Escriba una frase o palabra: ')
+palabra = palabra.lower()
+lista1 = list(palabra.replace(" ", ""))
+
+for caracter in set(lista1):
+    cantidad = lista1.count(caracter)
+    print(f'{caracter} : {cantidad}')
