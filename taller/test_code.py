97e4fcace158bff6f2e2f2d249998f8fe19f52bc
Davebc22 <ederd.barreroc@konradlorenz.edu.co>
2026-08-31 12:20:12 -0500

    Perímetro y área

--- taller/test_code.py
+++ taller/test_code.py
@@ -1,13 +1,23 @@
 """
-# 6. Realiza un algoritmo que cuente la cantidad de ocurrencias de cada caracter en una frase
+# 7. Realiza un algoritmo que tome como entrada 3 puntos x,y del plano cartesiano, formando un triangulo, como salida el programa calcule al area y perimetro
 
-
+Algoritmo
+Tomar 3 coordenadas
+Calcular pitágoras
+Usar forma vectorial del área
 """
+import numpy as np
 
-palabra = input('Escriba una frase o palabra: ')
-palabra = palabra.lower()
-lista1 = list(palabra.replace(" ", ""))
+x1 = np.array((2,0))
+x2 = np.array((1,5))
+x3 = np.array((2,1))
 
-for caracter in set(lista1):
-    cantidad = lista1.count(caracter)
-    print(f'{caracter} : {cantidad}')
+perimetro = np.linalg.norm(x2-x1) + np.linalg.norm(x3-x2) + np.linalg.norm(x3-x1)
+
+v1 = x2 - x1 
+v2 = x3 - x1 
+
+#Multiplicación cruzada
+area = 0.5 * np.abs(v1[0]*v2[1] - v1[1] * v2[0])
+
+print(f'El perímetro es {perimetro} y el área es: {area}')
