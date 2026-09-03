2b2b5fe8acfd7e3760f828be8847858a658ba545
Davebc22 <ederd.barreroc@konradlorenz.edu.co>
2026-09-03 11:49:29 -0500

    Collatz - Final ex out of 10

--- taller/test_code.py
+++ taller/test_code.py
@@ -1,18 +1,39 @@
 """
-### Implementar la codificación Run-Length Encoding (RLE) expandida
-Convierte la cadena "AAABBCDDDD" en "A3B2C1D4"
+# 10. Conjetura de Collatz
+
+### Implementa un algoritmo que genere y grafique la siguiente secuencia, iniciando desde un numero entero positivo cualquiera
+
+- Si el ultimo numero n es par, el siguiente es: n/2
+- Si el ultimo numero n es impar, el siguiente es: 3*n+1
+
 
 """
+import matplotlib.pyplot as plt
+# Función de Collatz
+def collatz(n):
+    if n<=0:
+        return []
+        
+    contenedor = [n]
+    while n !=1:
+        if n % 2 == 0:
+            n = n//2
+            contenedor.append(n)
+        else:
+            n = 3*n+1
+            contenedor.append(n)
+    return contenedor
 
-cadena = 'AAABBCDDDD'
-contador = 1
-acumulador = []
+#Pedir Datos 
+numero = int(input('Inserte un entero positivo: '))
+print(collatz(numero))
 
-for i in range(len(cadena)):
-    if i+1 < len(cadena) and cadena[i] == cadena[i+1]:
-        contador+=1
-    else:
-        acumulador.append(f'{cadena[i]}{contador}')
-        contador = 1
 
-print("".join(acumulador))
+# Graficar Función 
+datos = collatz(numero)
+plt.figure(figsize=(8,4))
+plt.plot(datos, color="#602b8f", linewidth=2, marker='o', markersize=4)
+plt.title('Conjetura de Collatz', fontsize=12, fontweight='semibold')
+plt.xlabel('Iteraciones')
+plt.ylabel('Valor ($n$)')
+plt.show()
