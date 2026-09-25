87e743a647e7a6b1c246071119835cb4666de45b
Davebc22 <ederd.barreroc@konradlorenz.edu.co>
2026-09-25 16:32:55 -0500

    Taller terminado

--- taller2/oop.py
+++ taller2/oop.py
@@ -1,1 +1,87 @@
-print("Hello World")
+import random
+
+class Carta:
+
+    palo = {
+        "Treboles" : 1,
+        "Diamantes" : 2,
+        "Picas" : 3, 
+        "Corazones" : 4    
+    }
+
+    identificador ={ 
+        "A": 1, 
+        "2": 2,
+        "3": 3, 
+        "4": 4, 
+        "5": 5, 
+        "6": 6, 
+        "7": 7, 
+        "8": 8, 
+        "9": 9, 
+        "10": 10, 
+        "J": 11, 
+        "Q": 12, 
+        "K": 13
+    }
+
+    def __init__(self, palo, identificador):
+        self.palo = palo
+        self.identificador = identificador
+
+    def __str__(self):
+        return f'{self.identificador} de {self.palo}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, otra):
+        return self.palo == otra.palo and self.identificador == otra.identificador
+
+    def __lt__(self, otra):
+        #C1
+        palo_self = Carta.palo[self.palo]
+        id_self = Carta.identificador[str(self.identificador)]
+        #C2
+        palo_otra = Carta.palo[otra.palo]
+        id_otra = Carta.identificador[str(otra.identificador)]
+        
+        if palo_self < palo_otra:
+            return True
+        elif palo_self > palo_otra:
+            return False
+        else: 
+            return id_self < id_otra
+
+class Baraja:
+    def __init__(self):
+        self.baraja = []
+        for palo in Carta.palo:
+            for identificador in Carta.identificador:
+                carta = Carta(palo,identificador)
+                self.baraja.append(carta)
+
+    def __str__(self):
+        return f'Baraja con {len(self.baraja)} cartas: \n\n{self.baraja}'
+
+    def barajar(self):
+        random.shuffle(self.baraja)
+
+    def ordenar(self):
+        self.baraja.sort()
+
+
+# Pruebas 
+
+
+baraja = Baraja()
+print("### NUEVA BARAJA ###")
+print(f'{baraja}\n')
+
+baraja.barajar()
+print("### BARAJANDO ###")
+print(f'\n{baraja}\n')
+
+baraja.ordenar
+print("### ORGANIZANDO ###")
+print(f'\n{baraja}\n')
